7b96ab6f330adc6b33e0c13dbdda0fa84c9dee33
Denis Simonovski <simonovskidenis3@gmail.com>
2018-12-06 21:54:11 +0100

    AWS API Test
    Signed-off-by: Denis Simonovski <simonovskidenis3@gmail.com>

---
+++ mysql/sql-scripts/CreateTable.sql
@@ -0,0 +1,7 @@
+CREATE TABLE Users (
+Id int NOT NULL AUTO_INCREMENT,
+Name varchar(25),
+Gender  varchar(25),
+Age INTEGER,
+PRIMARY KEY (Id)
+);
---
+++ mysql/sql-scripts/InsertData.sql
@@ -0,0 +1,8 @@
+INSERT INTO Users
+(Id, Name, Gender, Age)
+VALUES
+(1, "Ivana Jakimovska", "F", 39),
+(2, "Bojan Todorovski", "M", 28),
+(3, "Biljana Naumoska", "F", 27),
+(4, "Daniel Mladenovski", "M", 47),
+(5, "Dijana Stefanovska", "F", 25);
