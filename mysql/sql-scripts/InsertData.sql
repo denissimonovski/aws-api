670e40f9297a2b07e203f584a348a35f77b78f51
Denis Simonovski <simonovskidenis3@gmail.com>
2018-12-08 19:02:03 +0100

    AWS API Test
    Signed-off-by: Denis Simonovski <simonovskidenis3@gmail.com>

--- mysql/sql-scripts/InsertData.sql
+++ mysql/sql-scripts/InsertData.sql
@@ -1,8 +1,8 @@
 INSERT INTO Users
 (Id, Name, Gender, Age)
 VALUES
-(1, "Ivana Jakimovska", "F", 39),
-(2, "Bojan Todorovski", "M", 28),
-(3, "Biljana Naumoska", "F", 27),
+(1, "Jovana Ilievska", "F", 39),
+(2, "Dimitar Kirilov", "M", 28),
+(3, "Marija Kuzmanovska", "F", 27),
 (4, "Daniel Mladenovski", "M", 47),
-(5, "Dijana Stefanovska", "F", 25);
+(5, "Ivana Stefanovska", "F", 25);
